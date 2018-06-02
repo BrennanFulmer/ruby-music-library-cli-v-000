93ea4df448eb51561d386d535bcb121440e980c0
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-02 02:21:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ class Artist
   extend Common::ClassMethods, Concerns::Findable
   include Common::InstanceMethods
 
-  attr_accessor :name, :songs
+  attr_accessor :name, 
+  attr_reader :songs
   @@all = []
 
   def initialize(name)
