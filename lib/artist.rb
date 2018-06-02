e1b4a414c4c62a264bd2df393102951d6115d927
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-02 02:29:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
   end
   
   def add_song(song)
-    song.instance_variable_set(:@artist, self) unless song.artist
+    song.artist = self unless song.artist
     songs << song unless songs.include?(song)
   end
   
